1202dc95034ca825f2c4a3fa52d9d0f6d339d134
pamelacc <pamela.costa.carvalho@usp.br>
2025-12-12 14:56:59 +0100

    adapt eos from emto

--- modules/inputs/__init__.py
+++ modules/inputs/__init__.py
@@ -15,7 +15,7 @@ from .kstr_from_cif import create_kstr_input_from_cif  # NEW
 from .shape import create_shape_input
 from .kgrn import create_kgrn_input_fm, create_kgrn_input_afm, create_kgrn_input_afm_dlm, create_kgrn_input_pm
 from .kfcd import create_kfcd_input
-from .eos_emto import create_eos_input
+from .eos_emto import create_eos_input, parse_eos_output, morse_energy
 from .jobs_tetralith import (
     create_job_ca,
     create_job_volume,
@@ -37,4 +37,6 @@ __all__ = [
     'create_job_volume',
     'write_serial_sbatch',
     'write_parallel_sbatch',
+    'parse_eos_output',
+    'morse_energy'
 ]
